c79919c052dee3d76b6e7e7beb6d5cfec173f5e0
prudhviraj1541 <prudhviraj.l@way2online.co.in>
2021-03-30 16:34:03 +0530

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  @ViewChild('bookForm') bookstore: any;
   title = 'task-logis';
+  BookDetails: any = {}
+  constructor() { }
+  bookStote: any = {
+    title: '',
+    author: '',
+    website: ''
+  }
+  final: any = []
+  ngOnInit(): void {
+
+  }
+  OnFormSubmit(form: any) {
+    // console.log(form);
+    if (form.valid) {
+      this.BookDetails = Object.assign({},this.bookStote);
+      this.final.push(this.BookDetails);
+      // console.log(this.final);
+    }
+  }
+  reset() {
+    // console.log(this.bookstore);
+    this.bookstore.reset();
+  }
 }
